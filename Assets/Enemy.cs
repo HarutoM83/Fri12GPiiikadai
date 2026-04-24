0c7a4e62ce56a94dc15361e3110c51cc03d1b48d
raytrek2-\ktc <>
2026-04-24 12:24:02 +0900

    接地制御、ジャンプ制限、敵の動き少々、アニメーション(ジャンプ少々)

---
+++ Assets/Enemy.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float enemyspeed;
+
+    Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocityX = enemyspeed;
+    }
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,19 +1,23 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] float jumpSpeed;
+    bool isGrounded = false;
 
     PlayerInput playerInput;
     Rigidbody2D rb;
+    Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,10 +26,38 @@ public class Player : MonoBehaviour
         var move = playerInput.actions["Move"].ReadValue<Vector2>();
 
         rb.linearVelocityX = move.x * speed;
+        animator.SetBool("Walk", true);
 
-        if (playerInput.actions["Jump"].WasPressedThisFrame())
+        if (playerInput.actions["Jump"].WasPressedThisFrame() && isGrounded)
         {
             rb.linearVelocityY = jumpSpeed;
+            isGrounded = false;
+            animator.SetTrigger("Jump");
+        }
+    }
+    private void FixedUpdate()
+    {
+        isGrounded = false;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision) 
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        { 
+            isGrounded = false;
         }
     }
 }
